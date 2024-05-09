380a10e381f96633a02720ac20ddae02114d9d32
leonjye <leonjye@ntub.edu.tw>
2024-05-09 14:19:11 +0800

    Fragment 切換範例

---
+++ app/src/main/java/imd/ntub/myfrags0509/MainActivity.kt
@@ -0,0 +1,39 @@
+package imd.ntub.myfrags0509
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        class ViewPagerAdater(activity: MainActivity): FragmentStateAdapter(activity){
+            override fun getItemCount() = 3
+
+            override fun createFragment(position: Int) =
+                when(position){
+                    0-> FirstFragment.newInstance()
+                    1-> SecondFragment.newInstance()
+                    2-> ThirdFragment.newInstance("","")
+                    else-> FirstFragment.newInstance()
+                }
+        }
+        // 程式: https://github.com/leonjyentub/MyFrags0509
+        val viewPager = findViewById<ViewPager2>(R.id.viewpager)
+        viewPager.adapter = ViewPagerAdater(this)
+        findViewById<Button>(R.id.btn1).setOnClickListener {
+            viewPager.currentItem = 0
+        }
+        findViewById<Button>(R.id.btn2).setOnClickListener {
+            viewPager.setCurrentItem(1, true)
+        }
+        findViewById<Button>(R.id.btn3).setOnClickListener {
+            viewPager.setCurrentItem(2, true)
+        }
+    }
+}
